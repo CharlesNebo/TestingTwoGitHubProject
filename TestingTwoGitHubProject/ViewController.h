e43a1702370d0c6cf3b080c9b5ed8f043704fc9f
Charles Nebo <n1nech00@students.oamk.fi>
2014-09-03 16:14:42 +0300

    Our initial commit
    Created  the second Xcode project

---
+++ TestingTwoGitHubProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestingTwoGitHubProject
+//
+//  Created by Charles Nebo on 03/09/14.
+//  Copyright (c) 2014 charles Nebo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestingTwoGitHubProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestingTwoGitHubProject
+//
+//  Created by Charles Nebo on 03/09/14.
+//  Copyright (c) 2014 charles Nebo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
